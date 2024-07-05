daa80a9752b291c8fe14f0eb5fc84d869d4c50f9
Nikita Antonov <nikita45454@gmail.com>
2024-07-05 13:41:04 +0500

    feat: встраивает pinia

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import App from './App.vue';
 import './app/boot/registerComponents';
 import './assets/scss/app.scss';
 import { router } from './app/router';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app
 .use(router)
+.use(pinia)
 .mount('#app');
