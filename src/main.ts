5e268f3122b65faf93e5e485fb82bb11ecea4597
Nikita Antonov <nikita45454@gmail.com>
2024-07-06 00:00:07 +0500

    feat: встраивает vite-plugin-svg-icons

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './assets/scss/app.scss';
+import 'virtual:svg-icons-register';
 import createApp from './app/lib/createApp';
 
 createApp();
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
     ]
-  }
+  },
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      symbolId: 'icon-[dir]-[name]',
+      inject: 'body-last',
+      customDomId: '__svg__icons__dom__',
+    }),
+  ],
 })
