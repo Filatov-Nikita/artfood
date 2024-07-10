d1433af001b5bb93e6cdfb69ad1ecf0821f54e86
Nikita Antonov <nikita45454@gmail.com>
2024-07-10 14:21:48 +0300

    feat: добавляет widget Header

---
+++ src/widgets/Header/model/contacts.ts
@@ -0,0 +1,9 @@
+export default {
+  worktime: '10:00-20:00',
+  wa: {
+    path: '#'
+  },
+  tg: {
+    path: '#',
+  }
+}
---
+++ src/widgets/Header/ui/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <header class="header">
+    <div class="header__wrap">
+      <div class="header__head">
+        <div class="header__links">
+          <a class="header__link header__link--wa" :href="contacts.wa.path" target="_blank">WhatsApp</a>
+          <a class="header__link header__link--tg" :href="contacts.tg.path" target="_blank">Telegram</a>
+        </div>
+        <span class="header__separator">∙</span>
+        <div class="header__time">
+          Время работы: {{ contacts.worktime }}
+        </div>
+      </div>
+      <div class="header__toolbar">
+        <router-link class="header__logo-link" :to="{ name: 'home' }">
+          <AppLogo class="header__logo" />
+        </router-link>
+        <div class="header__actions">
+          <ButtonRound size="40px" icon="shopping-cart-regular"></ButtonRound>
+          <ButtonRound
+            size="40px"
+            :icon="showedMenu ? 'x-regular' : 'list-regular'"
+            @click="$emit('update:showedMenu', !showedMenu)"
+          ></ButtonRound>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+  import contacts from '../model/contacts';
+
+  defineProps<{
+    showedMenu: boolean,
+  }>();
+
+  defineEmits<{
+    (event: 'update:showedMenu', value: boolean): void,
+  }>();
+</script>
+
+<style scoped lang="scss">
+  .header {
+    @apply tw-bg-neutral tw-rounded-xl;
+
+    &__wrap {
+      padding: 8px 16px;
+    }
+
+    &__head {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      gap: 4px;
+      padding-bottom: 12px;
+      border-bottom: 1px solid theme('colors.border01');
+      @apply tw-text-body-xs-medium;
+    }
+
+    &__links {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 8px;
+    }
+
+    &__link {
+      &--wa {
+        @apply tw-text-primary;
+      }
+
+      &--tg {
+        color: #19A1E6;
+      }
+    }
+
+    &__time {
+      @apply tw-text-text00;
+    }
+
+    &__separator {
+      @apply tw-text-text01;
+    }
+
+    &__toolbar {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: space-between;
+      gap: 20px;
+      padding-top: 12px;
+    }
+
+    &__logo {
+      width: 107px;
+    }
+
+    &__actions {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      gap: 8px;
+    }
+  }
+</style>
