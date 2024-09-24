65e77b8a296efd88930c0882de8e7d8fe35d72c4
Nikita Antonov <nikita45454@gmail.com>
2024-09-24 18:50:28 +0300

    feat: yup - добавляет русский язык

--- src/app/boot/registerValidationRules.ts
+++ src/app/boot/registerValidationRules.ts
@@ -3,8 +3,15 @@ import { defineRule, configure } from 'vee-validate';
 import { localize } from '@vee-validate/i18n';
 import { required, email } from '@vee-validate/rules';
 import ru from '@vee-validate/i18n/dist/locale/ru.json';
+import { ru as yupRu } from 'yup-locales';
+import { setLocale as yupLocale } from 'yup';
 
 export default boot(() => {
+  yupLocale({
+    ...yupRu,
+    mixed: { required: '${path} - обязательное поле' },
+  });
+
   configure({
     generateMessage: localize({
       ru,
