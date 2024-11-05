fa7ec8e65566ceaf3bfb8f5678e51de6c674e273
Nikita Antonov <nikita45454@gmail.com>
2024-11-05 15:32:01 +0500

    feat: programs - добавляет CardItem

---
+++ src/entities/programs/ui/CardItem/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <article>
+    <div class="img-wrap">
+      <AppImg class="img" :src="item.img" />
+    </div>
+    <p class="name">{{ item.name }}</p>
+    <p class="text">{{ item.text }}</p>
+    <div class="price-wrap">
+      <p class="price">{{ item.price }}</p>
+      <p class="days"><AppPlural :cases="cases" :total="+item.days" /></p>
+    </div>
+  </article>
+</template>
+
+<script setup lang="ts">
+  import type { ProgramsElement } from '@/shared/repositories/programs';
+
+  defineProps<{
+    item: ProgramsElement,
+  }>();
+
+  const cases: [ string, string, string ] = [ 'день', 'дня', 'дней' ];
+</script>
+
+<style scoped lang="scss">
+  .img-wrap {
+    width: 100%;
+    height: 343px;
+    overflow: hidden;
+    cursor: pointer;
+    @apply tw-rounded-1.5xl tw-mb-12;
+
+    @include lg {
+      height: 320px;
+    }
+  }
+
+  .img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .name {
+    @apply tw-text-body-m-medium tw-mb-4;
+  }
+
+  .text {
+    @apply tw-text-body-s-regular tw-text-text01 tw-mb-12;
+  }
+
+  .price {
+    @apply tw-text-body-l-bold -tw-tracking-2 tw-mb-4;
+  }
+
+  .days {
+    @apply tw-text-body-xs-medium tw-text-text01;
+  }
+</style>
--- src/entities/programs/ui/ProgramList.vue
+++ src/entities/programs/ui/ProgramList.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="prog-list">
-    <ProgramListItem
+    <CardItem
       class="prog-item"
       v-for="item in items"
       :key="item.id"
       :item="item"
-      @click="$emit('change:program', item)"
+      @click="$emit('change:program', item.id)"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-  import ProgramListItem from './ProgramListItem.vue';
+  import CardItem from './CardItem/index.vue';
   import type { ProgramsElement } from '@/shared/repositories/programs';
 
   defineProps<{
@@ -19,7 +19,7 @@
   }>();
 
   const emit = defineEmits<{
-    (event: 'change:program', program: ProgramsElement): void,
+    (event: 'change:program', programId: string): void,
   }>();
 </script>
 
@@ -33,5 +33,13 @@
   .prog-item {
     margin: 20px 10px;
     width: calc(100% - 20px);
+
+    @include lg {
+      width: calc(33.3% - 20px);
+    }
+
+    @include xl {
+      width: calc(25% - 20px);
+    }
   }
 </style>
