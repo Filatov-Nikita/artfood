5cc0014ce7a01fbfd8297272432ae65687f1d1b7
Nikita Antonov <nikita45454@gmail.com>
2024-11-08 00:50:53 +0500

    feat: добавляет CateringPage

--- src/pages/catering/ui/CateringPage.vue
+++ src/pages/catering/ui/CateringPage.vue
@@ -1,11 +1,13 @@
 <template>
   <main class="page--py">
     <div class="wrapper">
-      1
+      <InnerSection />
+      <AboutSection />
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
-
+  import InnerSection from './InnerSection/index.vue';
+  import AboutSection from './AboutSection/index.vue';
 </script>
