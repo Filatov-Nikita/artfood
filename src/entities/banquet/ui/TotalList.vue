03604d74e01c79b32a4f7239a19aea49d70407b9
Nikita Antonov <nikita45454@gmail.com>
2024-11-28 14:47:04 +0500

    feat: banquet - добавляет адаптив TotalList

--- src/entities/banquet/ui/TotalList.vue
+++ src/entities/banquet/ui/TotalList.vue
@@ -7,6 +7,11 @@
         {{ row[0].name }}
       </div>
       <div class="items" v-else>
+        <div class="head" v-if="grid.lg">
+          <p>Блюдо</p>
+          <p>Вес, г</p>
+          <p>Цена</p>
+        </div>
         <template
           v-for="item in row"
         >
@@ -26,12 +31,15 @@
 <script setup lang="ts">
   import type { Total, TotalSection, TotalItem } from '@/shared/repositories/banquet';
   import { computed } from 'vue';
+  import { useAppGrid } from '@/shared/lib/useScreen';
 
   type Items = ([ TotalSection & { type: 'section' } ] | (TotalItem & { type: 'item' })[])[];
 
   const props = defineProps<{
     total: Total,
   }>();
+
+  const grid = useAppGrid();
 
   const data = computed(() => {
     const items: Items = [];
@@ -58,10 +66,18 @@
 <style scoped lang="scss">
   .section-name {
     @apply tw-text-body-l-bold -tw-tracking-2 tw-mb-8;
+
+    @include lg {
+      @apply tw-text-heading-3-bold tw-mb-12;
+    }
   }
 
   .items {
     @apply tw-mb-24;
+
+    @include lg {
+      @apply tw-mb-40;
+    }
   }
 
   .item {
@@ -70,14 +86,41 @@
     &__name {
       margin-bottom: 2px;
       @apply tw-text-body-s-medium;
+
+      @include lg {
+        @apply tw-text-body-m-regular tw-mb-0;
+      }
     }
 
     &__ves {
       @apply tw-text-text02 tw-text-body-xs-regular tw-mb-8;
+
+      @include lg {
+        @apply tw-text-body-m-regular tw-text-text00 tw-mb-0;
+      }
     }
 
     &__price {
       @apply tw-text-body-m-bold -tw-tracking-2;
+
+      @include lg {
+        @apply tw-text-body-m-regular;
+      }
+    }
+  }
+
+  .head, .item {
+    @include lg {
+      display: grid;
+      grid-template-columns: 1fr 88px 88px;
+      column-gap: 20px;
+      @apply tw-py-16;
+    }
+  }
+
+  .head {
+    @include lg {
+      @apply tw-text-text01 tw-text-body-m-regular tw-border-b tw-border-solid tw-border-border01;
     }
   }
 </style>
