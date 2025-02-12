9bff939841e40d6b1b3be479737f455832d571d3
Nikita Antonov <nikita45454@gmail.com>
2025-02-12 16:49:42 +0300

    обновляет контакты

--- src/widgets/Footer/model/contacts.ts
+++ src/widgets/Footer/model/contacts.ts
@@ -1,7 +1,7 @@
 export default {
-  address: 'Уфа, ул. Гагарина, 45',
+  address: 'Уфа, ул. Бессонова, 28А',
   worktime: 'Пн-Вс, 10:00-20:00',
-  email: 'info@artfood.ru',
+  email: 'ufa.artfood@gmail.com',
   phone: '+7 (347) 285-00-07',
   wa: 'https://wa.me/+79272369407',
 }
